927e8ac5e16b97d3b0e6e4a7699876ec3263b9d4
SatoShun <shun.sato1@gmail.com>
2017-12-30 03:26:40 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/droidkaigi2018/proguard/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.satoshun.droidkaigi2018.proguard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_act)
+  }
+}
