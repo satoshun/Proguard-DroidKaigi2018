ae7fa315a16b84748e8e750e24f5da374ee99bf5
SatoShun <shun.sato1@gmail.com>
2018-01-30 13:05:14 +0000

    add sample

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -23,4 +23,72 @@
 #-dontwarn javax.annotation.**
 #-dontwarn org.codehaus.mojo.animal_sniffer.**
 
+
+#-dontwarn okio.**
+-dontwarn javax.annotation.Nullable
+-dontwarn javax.annotation.ParametersAreNonnullByDefault
+-dontwarn org.codehaus.mojo.animal_sniffer.*
+#-dontwarn org.codehaus.mojo.**
+
+
+# -dontshrink
+
+#-keep class com.github.satoshun.droidkaigi2018.proguard.MainActivity {
+#    *** oooooo(int);
+#}
+
+# クラス名、メンバーをキープ
+# 使っていなくても指定があればキープ
+#-keep class com.github.satoshun.droidkaigi2018.proguard.Hoge {
+#    *;
+#         android.view.View view2;
+#}
+
+# メンバーをキープ。クラス名は短縮
+# 使っていない場合もキープ。
+#-keepclassmembers class com.github.satoshun.droidkaigi2018.proguard.Hoge {
+#    *;
+#}
+-keep class com.github.satoshun.droidkaigi2018.proguard.Hogegs
+-keep @com.github.satoshun.droidkaigi2018.proguard.Hogegs class * {*;}
+-keepclasseswithmembers class * {
+    @com.github.satoshun.droidkaigi2018.proguard.Hogegs <methods>;
+}
+-keepclasseswithmembers class * {
+    @com.github.satoshun.droidkaigi2018.proguard.Hogegs <fields>;
+}
+-keepclasseswithmembers class * {
+    @com.github.satoshun.droidkaigi2018.proguard.Hogegs <init>(...);
+}
+
+
+# メンバーをキープ。クラス名は短縮
+# 使っていない場合は削除。
+#-keepclassmembernames class com.github.satoshun.droidkaigi2018.proguard.Hoge {
+#    *;
+##    android.view.View view2;
+#}
+
+# クラス名、メンバーをキープ
+# 使っていない場合は指定されていても削除
+#-keepnames class com.github.satoshun.droidkaigi2018.proguard.Hoge {
+#    *;
+##    android.view.View view2;
+#}
+
+#-dontwarn
+
+# Gson
+#-keepattributes Signature
+# For using GSON @Expose annotation
+#-keepattributes *Annotation*
+
 -dontwarn okio.**
+-dontwarn javax.annotation.**
+#このアノテーションがついているメソッドを消さない(ただしclassが参照されている場合のみ)
+-keepclasseswithmembers class * {
+    @com.squareup.moshi.* <methods>;
+}
+#JsonQualifierを継承しているannotationを消さない
+#Javaから参照されていない場合はstripされる
+-keep @com.squareup.moshi.JsonQualifier interface *
--- app/src/main/java/com/github/satoshun/droidkaigi2018/proguard/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/droidkaigi2018/proguard/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.github.satoshun.droidkaigi2018.proguard
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.JsonQualifier
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.ToJson
+
+
+//import javax.annotation.Nullable
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_act)
+
+    val moshi = Moshi.Builder()
+        .add(ColorAdapter())
+        .build()
+    val jsonAdapter = moshi.adapter(Hoge::class.java)
+    val hoge = jsonAdapter.fromJson("")
+
+    // retain HexColor annotation and all with methods
+//    val color = HexColor::class
+//    println(color)
+
+//    val hoge = Hoge()
+//    hoge.niko()
+//    hoge.args(10)
+//    oooooo(HHHHHH())
+  }
+
+//  fun oooooo(view: HHHHHH) {
+//    println("HOGE: ${view.javaClass.annotations.size}")
+//
+//    view.javaClass.annotations.forEach {
+//      println("HOGE: ${it}")
+//
+//      if (it is Hogegs) {
+//        Log.d("Log", "find Hogegs")
+//      }
+//      if (it is Nullable) {
+//        Log.d("Log", "find Nullable")
+//      }
+//    }
+//  }
+}
+
+
+class Hoge {
+  private val abc: String = ""
+  private lateinit var view: View
+  private lateinit var view2: View
+
+  @Hogegs
+  fun fuga() {
+    println(abc)
+  }
+
+  fun niko() {
+    println(view)
+  }
+
+//  fun args(a: Int = 10, b: Int = 20) {
+//    println(a + b)
+//  }
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@JsonQualifier
+annotation class HexColor
+
+annotation class Hogegs
+
+//@Nullable
+//class HHHHHH
+
+class ColorAdapter {
+  @ToJson fun toJson(@HexColor rgb: Int): String {
+    return String.format("#%06x", rgb)
+  }
+
+  @FromJson @HexColor fun fromJson(rgb: String): Int {
+    return Integer.parseInt(rgb.substring(1), 16)
   }
 }
